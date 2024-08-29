1a6d033bf35ab214be11e50fdbfc70796569cf7c
cristigb <isafried@hotmail.com>
2024-08-29 16:44:33 -0500

    user login and logout

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'list_product'
+LOGOUT_REDIRECT_URL = 'list_product'
--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('usuarios/', include('users.urls')),
     path('productos/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
